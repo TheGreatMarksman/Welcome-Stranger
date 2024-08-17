9ca48d28970797f55c42fa7247c66d18d0dc9ebf
TheGreatMarksman <marcusedooley@yahoo.com>
2024-08-17 09:45:37 -0700

    Split up the screen into sections, and added a searchbar and list that lists the organizations

--- app/views.py
+++ app/views.py
@@ -6,4 +6,5 @@ blueprint = Blueprint("views", __name__)
 @blueprint.route("/")
 def home():
     organizations = db.query_db("SELECT * FROM organizations")
+    print(organizations)
     return render_template("index.html", organizations=organizations)
