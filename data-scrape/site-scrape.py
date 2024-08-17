c3e40d7069eaf0e95be3e8bb8fa3bc9453d5a78a
Lucy <lucyziwenzhou@gmail.com>
2024-08-17 16:39:18 -0700

    divide filtering and site scraping into two files

--- data-scrape/site-scrape.py
+++ data-scrape/site-scrape.py
@@ -2,6 +2,15 @@
 # split into two databases -> those with websites and those without
 # search for keywords for those with websites and tag -> if broken link, move to no website
 # somehow deal with those without websites -> google search???
+import urllib.request
+site_content = urllib.request.urlopen(link).read().decode("utf-8")
+
+keywords = ['word', 'word1', 'word2']
+for word in keywords:
+    if word in site_content:
+        print(f"{word} found")
+    else:
+        print(f"{word} not found")
 
 
 filter = ["jehova", "jehova's witness", "mormon", "latter day", "latter day saints", "joseph smith", "lds", "god the mother", 
