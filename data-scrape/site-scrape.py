ca9b1d966a86dbdd61a4f6bc4ce73daefdbd66ac
Lucy <lucyziwenzhou@gmail.com>
2024-08-17 17:54:33 -0700

    implemented flag parsing successfully

--- data-scrape/site-scrape.py
+++ data-scrape/site-scrape.py
@@ -2,6 +2,30 @@
 # split into two databases -> those with websites and those without
 # search for keywords for those with websites and tag -> if broken link, move to no website
 # somehow deal with those without websites -> google search???
+import pandas as pd
+import string
+
+# for importing flags
+def getNames(list):
+    val = []
+    for s in list:
+        name = s.split(',')
+        for n in name: 
+            val.append(n)
+    return val
+
+# import flags
+temp = pd.read_csv('data-scrape/flags.csv', usecols=[1])["Nation"].to_list()
+nation = getNames(temp)
+temp = pd.read_csv('data-scrape/flags.csv', usecols=[2])["People"].to_list()
+people = getNames(temp)
+temp = pd.read_csv('data-scrape/flags.csv', usecols=[3])["Languages"].to_list()
+language = getNames(temp)
+
+flags = list(set(nation + people + language))
+print(flags)
+
+
 import urllib.request
 site_content = urllib.request.urlopen(link).read().decode("utf-8")
 
@@ -11,9 +35,5 @@ for word in keywords:
         print(f"{word} found")
     else:
         print(f"{word} not found")
-
-
-filter = ["jehova", "jehova's witness", "mormon", "latter day", "latter day saints", "joseph smith", "lds", "god the mother", 
-          "jéhova", "mormone", "mormonisme", "saints des derniers jours", "derniers jours", "dieu la mère"]
 
 nolink = []
