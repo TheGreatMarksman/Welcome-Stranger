aa6bc029599244cd524c8be52c21d7570c59d901
Ellie <ellie_neufeld@sfu.ca>
2024-08-18 11:23:37 -0700

    added some documentation and renamed some variables

--- data-scrape/charity-link-scraper.py
+++ data-scrape/charity-link-scraper.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 
 # code adapted from: https://www.scrapehero.com/scrape-data-from-data-layer-of-google-tag-manager/
 
-MAX_NUM_SEARCH_RESULTS = "26000"
+MAX_NUM_SEARCH_RESULTS = "26000" # to get all results on the same page
 
 headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'}
 list_url = "https://www.charitydata.ca/search?fpeYear=2022&pageSize="+MAX_NUM_SEARCH_RESULTS+"&sortBy=relevance&sortDirection=asc&offset=0&descriptionCode=c&categoryCode=30"
@@ -12,25 +12,27 @@ Data = []
 filter_words = ["jehova", "jehova's witness", "mormon", "latter day", "latter day saints", "joseph smith", "lds", "god the mother", 
           "jéhova", "mormone", "mormonisme", "saints des derniers jours", "derniers jours", "dieu la mère"]
 
+# request the Charity Data search page
 driver = webdriver.Chrome()
 driver.implicitly_wait(100)
 driver.get(list_url)
 
 try:
-    elem = driver.find_elements(By.XPATH, "/html/body/main/div/div[2]/div/article/a")
-    for url in elem:
+    # get the name, Charity Data link, and description of each charity
+    elements = driver.find_elements(By.XPATH, "/html/body/main/div/div[2]/div/article/a")
+    for elem in elements:
         l = {}
-        all_text = url.text
+        all_text = elem.text
         all_text = all_text.splitlines()
         skip = False
         for word in filter_words:
-            for element in all_text:
-                if word in element:
+            for section in all_text:
+                if word in section:
                     skip = True
                     break
         if skip==False:
             l["name"] = all_text[0]
-            l["charity data link"] = url.get_attribute("href")
+            l["charity data link"] = elem.get_attribute("href")
             try: l["description"] = all_text[1]
             except: pass
             Data.append(l)
