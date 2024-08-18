06db2010fd16142427fddad6dd8e53add76f6be9
Ellie <ellie_neufeld@sfu.ca>
2024-08-18 11:38:47 -0700

    added some documentation

--- data-scrape/find-contact-page-charity-websites.py
+++ data-scrape/find-contact-page-charity-websites.py
@@ -2,7 +2,9 @@ import pandas as pd
 import requests
 import re
 
-orgs = pd.read_csv("first-pass.csv", usecols=[0,1], dtype={"name":"str", "link":"str"}) # name, link
+# this code is no longer used, but is kept for reference
+
+orgs = pd.read_csv("first-pass.csv", usecols=[0,1], dtype={"name":"str", "link":"str"}) # schema: name, link
 #orgs = orgs.head()
 org_links = orgs["link"].to_list()
 
@@ -11,12 +13,15 @@ contact_pages = []
 expression = re.compile(r"(.+\.(ca|com|org))")
 
 for url in org_links:
+    # for each link, append add-ons and see if the new link works
     success = False
     tries = [url+"contact/",url+"contact-us",url+"connect",url+"contactus",
              url+"/contact/",url+"/contact-us", url+"/connect",url+"/contactus"]
-    match = expression.search(url) # does not work if facebook page, for ex
+    # for each link, try to find the homepage and append add-ons to that
+    match = expression.search(url) # false positive for some links, ex facebook pages
     if match: tries.extend([match.group(1)+"/contact/",match.group(1)+"/contact-us",match.group(1)+"/connect",match.group(1)+"/contactus"])
     for try_url in tries:
+        # try requesting each link and stop if successful
         if success==True: break
         try:
             html = requests.get(try_url, headers=headers)
@@ -30,7 +35,7 @@ for url in org_links:
             pass
 
 
-contact_pages_df = pd.DataFrame(contact_pages, dtype="str") # link, contact page
+contact_pages_df = pd.DataFrame(contact_pages, dtype="str") # schema: link, contact page
 contact_pages_df.drop_duplicates(inplace=True)
 
 new_orgs = orgs.merge(contact_pages_df, on="link", how="left")
