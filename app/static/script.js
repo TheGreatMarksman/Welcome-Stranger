4eb55873836f41fed21dbedfad60c47dab5f17a3
TheGreatMarksman <marcusedooley@yahoo.com>
2024-08-18 11:05:13 -0700

    Added fullscreen button

--- app/static/script.js
+++ app/static/script.js
@@ -115,7 +115,20 @@ document.addEventListener('DOMContentLoaded', function () {
         document.getElementById('NationList').value = "";
         updateCityList();
         filterFromUI();
-    })
+    });
+
+    document.getElementById('fullscreen-btn').addEventListener('click', function() {
+        var iframe = document.getElementById('map');
+        if (iframe.requestFullscreen) {
+            iframe.requestFullscreen();
+        } else if (iframe.mozRequestFullScreen) { // Firefox
+            iframe.mozRequestFullScreen();
+        } else if (iframe.webkitRequestFullscreen) { // Chrome, Safari and Opera
+            iframe.webkitRequestFullscreen();
+        } else if (iframe.msRequestFullscreen) { // IE/Edge
+            iframe.msRequestFullscreen();
+        }
+    });
 });
 
 function updateCityList(province) {
